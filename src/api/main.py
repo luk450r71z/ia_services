affcc800a873b414dc30c52e2b28ed0a8bfbc459
Ortiz Lucas Emanuel <lucasdjbg@gmail.com>
2025-06-03 17:52:20 -0300

    fix(Structure) Changes

--- src/api/main.py
+++ src/api/main.py
@@ -29,4 +29,10 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        reload_dirs=["auth", "conversational_agent"]
+    )
